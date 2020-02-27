357be0d3e2c9e1d925fbef4876f3a180794c6a9e
udany <long.eagle.goodies@gmail.com>
2020-02-26 23:13:25 -0300

    Solution completion reporting

--- api/routes/remote/solution.js
+++ api/routes/remote/solution.js
@@ -4,7 +4,6 @@ import SolutionModel from "../../models/SolutionModel";
 import db from '../../Database';
 import Solution from '../../../shared/entities/Solution';
 import {DatabaseQueryCondition} from '../../js/DatabaseQueryComponent';
-import UserModel from "../../models/UserModel";
 
 let router = express.Router({});
 
@@ -31,6 +30,30 @@ router.get('/:world-:level', async (req, res) => {
 
     res.send(solution ? solution.Serialize() : null);
 
+});
+
+router.get('/:world-:level/exists', async (req, res) => {
+	if(!req.session.user){
+		res.status(404).send('Usuário não está logado.');
+		return;
+	}
+
+	let [solution] = await SolutionModel.select(db, [
+		new DatabaseQueryCondition({
+			column: 'world',
+			values: [req.params.world]
+		}),
+		new DatabaseQueryCondition({
+			column: 'level',
+			values: [req.params.level]
+		}),
+		new DatabaseQueryCondition({
+			column: 'userId',
+			values: [req.session.user.id]
+		})
+	]);
+
+	res.send({exists: !!solution, solution: solution});
 });
 
 router.post('/', async (req, res) => {
@@ -64,31 +87,6 @@ router.post('/', async (req, res) => {
     await SolutionModel.save(db, solution, ['code', 'userId', 'completion', 'world', 'level']);
 
     res.send(solution ? solution.Serialize() : null);
-
-});
-
-router.get('/:world-:level/exists', async (req, res) => {
-    if(!req.session.user){
-        res.status(404).send('Usuário não está logado.');
-        return;
-    }
-
-    let [solution] = await SolutionModel.select(db, [
-        new DatabaseQueryCondition({
-            column: 'world',
-            values: [req.params.world]
-        }),
-        new DatabaseQueryCondition({
-            column: 'level',
-            values: [req.params.level]
-        }),
-        new DatabaseQueryCondition({
-            column: 'userId',
-            values: [req.session.user.id]
-        })
-    ]);
-
-    res.send({exists: !!solution});
 
 });
 
